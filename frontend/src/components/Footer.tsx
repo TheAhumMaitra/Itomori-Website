1bc565b578b2b1f66664b28e5eca4d747904b2df
Ahum Maitra <theahummaitra@gmail.com>
2025-11-28 22:44:08 +0530

    Make footer sticky and improved the website

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <>
-      <div className="h-full w-full -4 mt-3 flex flex-col justify-center items-center font-bold p-3">
+      <div className="h-full w-full backdrop-blur-sm bottom-0 mt-3 flex flex-col justify-center items-center font-bold p-3">
         <h1>
-          This website&apos;s source code is open source is licensed under MIT
+          This website&apos;s source code is open source is licensed under GNU GENERAL PUBLIC LICENSE - VERSION 3
           License and content is licensed under
           <Link
             className="hover:italic hover:text-destructive ml-1"
--- frontend/src/components/ShowPreview.tsx
+++ frontend/src/components/ShowPreview.tsx
@@ -1,11 +1,14 @@
 export default function ShowPreview() {
   return (
     <>
-      <div className="w-full flex justify-center border-border border-4 items-center mt-4 flex-col gap-3 h-[60vh]">
+      <div className="w-full flex justify-center border-border border-4 items-center mt-4 flex-col gap-3 h-[80vh]">
         <h1 className="font-extrabold text-3xl underline font-sans">
           A brief introduction video:
         </h1>
-        <h3>Comming soon.....</h3>
+        <video width="700" height="700" controls preload="none">
+          <source src="public/Show_off.mp4" type="video/mp4" />
+          Sorry, I tried but your browser does not support the video tag. It's too old!
+        </video>
       </div>
     </>
   );
